7792d1e9fd04c1afa6a0f5a066439947ae8cbc14
Vladimir-Kod <kodvv@yandex.ru>
2022-11-24 18:14:08 +0500

    completed task 3(Escape Sequences in Strings)

---
+++ javascript/task-3-Escape-Sequences-in-Strings.js
@@ -0,0 +1,14 @@
+/*
+Escape Sequences in Strings
+Quotes are not the only characters that can be escaped inside a string. There are two reasons to use escaping characters:
+To allow you to use characters you may not otherwise be able to type out, such as a newline.
+To allow you to represent multiple quotes in a string without JavaScript misinterpreting what you mean.
+We learned this in the previous challenge.
+Assign the following three lines of text into the single variable myStr using escape sequences.
+
+FirstLine
+    \SecondLine
+ThirdLine
+*/
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
