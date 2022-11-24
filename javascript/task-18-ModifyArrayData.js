ccf73953542a285137cb965e374f809996ef865c
Vladimir-Kod <kodvv@yandex.ru>
2022-11-24 20:17:19 +0500

    completed task 18 (Modify Array Data)

---
+++ javascript/task-18-ModifyArrayData.js
@@ -0,0 +1,13 @@
+/*
+Modify Array Data With Indexes
+Unlike strings, the entries of arrays are mutable and can be changed freely, 
+even if the array was declared with const.
+
+myArray should now be [45, 64, 99].
+Waiting:You should be using correct index to modify the value in myArray.
+*/
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0]=45;
