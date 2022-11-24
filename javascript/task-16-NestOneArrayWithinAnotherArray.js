6b22bc2d60552787a423c0531e808fdcea5786bd
Vladimir-Kod <kodvv@yandex.ru>
2022-11-24 19:59:44 +0500

    completed task 16 (Nest one Array within)

---
+++ javascript/task-16-NestOneArrayWithinAnotherArray.js
@@ -0,0 +1,10 @@
+/*
+Nest one Array within Another Array
+You can also nest arrays within other arrays, like below:
+
+const teams = [["Bulls", 23], ["White Sox", 45]];
+This is also called a multi-dimensional array.
+
+myArray should have at least one array nested within another array.
+*/
+const myArray = [[123, "Java"],["script", 321]];
