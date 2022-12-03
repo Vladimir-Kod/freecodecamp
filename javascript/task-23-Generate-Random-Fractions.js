9da1916b18b351ba6b49305e365e640150ddca34
Vladimir-Kod <kodvv@yandex.ru>
2022-12-03 16:52:26 +0500

    completed task 23 (Generate Random Fractions)

---
+++ javascript/task-23-Generate-Random-Fractions.js
@@ -0,0 +1,14 @@
+// Generate Random Fractions with JavaScript
+
+// randomFraction should return a random number.
+// Waiting:The number returned by randomFraction should be a decimal.
+// Waiting:You should be using Math.random to generate the random decimal number.
+
+function randomFraction() {
+
+    // Only change code below this line
+  
+    return Math.random(1)
+  
+    // Only change code above this line
+}
